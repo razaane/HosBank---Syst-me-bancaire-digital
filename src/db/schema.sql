b90d5b3ad6688316f1464b45fc208f74c5130ce6
razaane <wakhidirazane@gmail.com>
2026-09-15 20:43:47 +0100

    SCRUM-17 #done Complete N-tiers directory structure and DB connection

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -96,7 +96,7 @@ CREATE TABLE demande (
     FOREIGN KEY (charge_client_id) REFERENCES charge_client(id) ON DELETE SET NULL
 );
 
-
+--still don't know to add this table or not 
 -- CREATE TABLE commentaire_demande (
 --     id INT AUTO_INCREMENT PRIMARY KEY,
 --     demande_id INT NOT NULL,
