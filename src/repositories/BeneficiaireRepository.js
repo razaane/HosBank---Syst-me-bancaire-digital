b2542a7ddf5623300bfa0b9af22fb455e1d83514
razaane <wakhidirazane@gmail.com>
2026-09-17 20:33:53 +0100

    j'ai ajouter le recherche par id et la creation d'un nouveau beneficiaire et aussi la suppresion dans le BeneficiaireRepository

--- src/repositories/BeneficiaireRepository.js
+++ src/repositories/BeneficiaireRepository.js
@@ -1,3 +1,25 @@
 const db = require("../db/connexion");
 const Beneficiaire =require("../models/Beneficiaire");
 
+async function findBeneficiaireById(clientId) {
+    const [rows] =await db.query('SELECT * FROM beneficiaire WHERE client_id=?',[clientId]);
+    return rows.map((row)=>new Beneficiaire(row));
+}   
+
+async function create(data) {
+    const {clientId,nom,rib,banque} = data;
+    const [resultat]=await db.query(
+        'INSERT INTO beneficiaire (client_id,nom,rib,banque) VALUES (?,?,?,?)'
+        ,[clientId,nom,rib,banque]);
+    return true;
+}
+
+async function deleteById(id) {
+     await db.query('DELETE FROM beneficiaire WHERE id=?',[id]);
+}
+
+module.exports={
+    findBeneficiaireById,
+    create,
+    deleteById,
+}
