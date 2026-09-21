c1c60d515c4f429697306161c8ca0ead960ddc3c
razaane <wakhidirazane@gmail.com>
2026-09-21 09:23:48 +0000

    HOS-123 l'ajout des routes pour les virements #done

--- src/routes/virementRoutes.js
+++ src/routes/virementRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const virementController = require("../controllers/virementController");
-
+//yzzjjakja
 router.get("/virement",virementController.list);
 router.get("/virement/effectuer",virementController.showAddForm);
 router.add("/virement",virementController.add);
