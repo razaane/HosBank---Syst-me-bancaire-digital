5a8d851fbad962d575536057b685fb57d23b0482
razaane <wakhidirazane@gmail.com>
2026-09-21 22:07:28 +0000

    HOS-137 Model creer demande et carte #done

---
+++ src/models/Demande.js
@@ -0,0 +1,14 @@
+class Demande {
+  constructor({ id, client_id, charge_client_id, type_demande, statut, date_creation, date_traitement, donnees_specifiques }) {
+    this.id = id;
+    this.clientId = client_id;
+    this.chargeClientId = charge_client_id;
+    this.typeDemande = type_demande;
+    this.statut = statut;
+    this.dateCreation = date_creation;
+    this.dateTraitement = date_traitement;
+    this.donneesSpecifiques = donnees_specifiques;
+  }
+}
+
+module.exports = Demande;
