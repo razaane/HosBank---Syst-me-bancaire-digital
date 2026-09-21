7d9e0d67389744e7feb3e221a2917d23795da9a6
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2026-09-21 16:33:36 +0000

    fix the middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,12 @@
+function authMiddleware(req, res, next) {
+
+
+    
+  if (req.session.userId) {
+    return next();
+  }
+
+  return res.redirect('/login');
+}
+
+module.exports = { authMiddleware };
