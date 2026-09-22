a0cde87558b9b4295dc039d8d2ed5186cee631bd
razaane <wakhidirazane@gmail.com>
2026-09-22 20:03:53 +0000

    HOS-157 ajout de controller  #Terminé

--- src/controllers/demandeController.js
+++ src/controllers/demandeController.js
@@ -55,4 +55,25 @@ async function opposerCarte(req, res) {
     res.status(400).send(err.message);
   }
 }
-module.exports = { listerMesDemandes, demanderCarteVirtuelle, showCarteVirtuelleForm ,showOppositionForm , opposerCarte};
+
+async function showPinForm(req, res) {
+  try {
+    const carteRepository = require('../repositories/carteRepository');
+    const cartes = await carteRepository.findByClientId(req.session.userId);
+    res.render('client/demande/renouvellement-pin', { cartes });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erreur lors du chargement de vos cartes.');
+  }
+}
+
+async function demanderRenouvellementPin(req, res) {
+  try {
+    const { carteId } = req.body;
+    await demandeService.demanderRenouvellementPin(req.session.userId, carteId);
+    res.redirect('/demandes');
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+}
+module.exports = { listerMesDemandes, demanderCarteVirtuelle, showCarteVirtuelleForm ,showOppositionForm , opposerCarte ,showPinForm,demanderRenouvellementPin};
