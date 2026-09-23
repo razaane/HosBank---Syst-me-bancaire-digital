baf6872a3fb717f70093923dc189b2f869993ec3
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2026-09-23 11:37:47 +0000

    HOS-329 change file #Terminé

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -2,6 +2,7 @@ const express=require('express')
 
 const router=express.Router()
 
+
 const {dashboardController}=require('../controllers/dashboardController')
 
 const {authMiddleware}=require('../middlewares/authMiddleware')
