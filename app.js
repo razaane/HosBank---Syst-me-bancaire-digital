fe11e5ace4ae4ee2f3937bccf9367996972fd196
razaane <wakhidirazane@gmail.com>
2026-09-25 09:40:38 +0000

    ajouter les fichiers ejs pour les demandes et clients et les reclamations pour charge client

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use('/', require('./src/routes/compteRoutes'));
 app.use('/', require('./src/routes/carteRoutes'));;
 app.use('/', require('./src/routes/profilRoutes'));
 app.use('/', require('./src/routes/reclamationRoutes'));
+app.use('/', require('./src/routes/chargeClientRoutes'));
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
