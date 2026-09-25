a05201038a780a368cf12e82e177dc0cf9e1fe5b
razaane <wakhidirazane@gmail.com>
2026-09-25 09:26:16 +0000

    ajout du model de reclamations et sidebar de charge client

--- src/models/Reclamation.js
+++ src/models/Reclamation.js
@@ -2,10 +2,12 @@ class Reclamation {
   constructor({ id, client_id, charge_client_id, sujet, description, statut, date_creation, date_traitement }) {
     this.id = id;
     this.clientId = client_id;
+    this.chargeClientId = charge_client_id;
     this.sujet = sujet;
     this.description = description;
     this.statut = statut;
     this.dateCreation = date_creation;
+    this.dateTraitement = date_traitement;
   }
 }
 module.exports = Reclamation;
